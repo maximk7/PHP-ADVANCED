57de406dfea4e46ddc547d13fb843c0ccdfa19dd
Maxim Krymchansky <maxim.krymchansky@student.rocva.nl>
2021-10-11 16:23:05 +0200

    change

---
+++ 2-Loops/taak03/loops-foreach-deel1.php
@@ -0,0 +1,28 @@
+<?php
+
+$dagen = array("maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag", "zondag");
+
+foreach ($dagen as $maandag) {
+    echo $maandag. "<br/>";
+}
+foreach ($dagen as $dinsdag) {
+    echo $dinsdag. "<br/>";
+}
+foreach ($dagen as $woensdag) {
+    echo $woensdag. "<br/>";
+}
+foreach ($dagen as $donderdag) {
+    echo $donderdag. "<br/>";
+}
+foreach ($dagen as $vrijdag) {
+    echo $vrijdag. "<br/>";
+}
+foreach ($dagen as $zaterdag) {
+    echo $zaterdag. "<br/>";
+}
+foreach ($dagen as $zondag) {
+    echo $zondag. "<br/>";
+}
+
+
+?>
