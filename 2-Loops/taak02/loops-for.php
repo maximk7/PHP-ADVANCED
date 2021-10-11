8a7779782a6481381869dd4b79a24bf05b926f7a
Maxim Krymchansky <maxim.krymchansky@student.rocva.nl>
2021-10-11 14:55:49 +0200

    change

---
+++ 2-Loops/taak02/loops-for.php
@@ -0,0 +1,11 @@
+<?php
+
+for($leeftijd = 0 ; $leeftijd < 18; $leeftijd++){
+        echo "$leeftijd: is niet oud genoeg en mag niet stemmen <br />";
+         
+    }
+     echo "$leeftijd ik mag wel stemmen"
+
+    
+
+    ?>
