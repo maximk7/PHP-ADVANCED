eb1679589769394d5830af66337e4f08df0da7c2
Maxim Krymchansky <maxim.krymchansky@student.rocva.nl>
2021-11-25 10:08:14 +0100

    change

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+      <title>mijngegevens</title>
+
+</head>
+
+<body>
+
+
+<?php
+
+
+
+echo"voornaam is:".$_POST["voornaam"];
+
+echo"achternaam is:".$_POST["achternaam"];
+
+echo"klas is:".$_POST["klas"];
+
+echo"leeftijd is:".$_POST["leeftijd"];
+
+echo"adres is:".$_POST["adres"];
+
+echo"plaatsnaam is:".$_POST["plaatsnaam"];
+
+echo"favoriete muziek is:".$_POST["favorietemuziek"];
+
+?>
+
+</body>
+
+</html>
